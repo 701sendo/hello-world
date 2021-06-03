559851e848f6bc39e83ebf120d97c762a5c20577
JANG <JANG@192.168.25.55>
2021-06-03 22:57:22 +0900

    안녕 World 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] orgs) {
 		System.out.println("Hello World");		
 		System.out.println("Hi World");		
+		System.out.println("¾È³ç World");		
 	}
 }
